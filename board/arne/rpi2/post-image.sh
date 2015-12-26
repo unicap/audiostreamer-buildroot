070bfc41a8c2145fe19daad4cde13e4ca7cf21f6
Arne Caspari <arne@unicap-imaging.org>
2015-12-26 13:56:48 +0100

    Install correct config.txt in output image dir

--- board/arne/rpi2/post-image.sh
+++ board/arne/rpi2/post-image.sh
@@ -5,3 +5,7 @@ set -e
 # Call mkknlimg to install the Device Tree
 "${HOST_DIR}/usr/bin/mkknlimg" "${BINARIES_DIR}/zImage" "${BINARIES_DIR}/zImage-dt"
 mv "${BINARIES_DIR}/zImage-dt" "${BINARIES_DIR}/zImage"
+
+echo "Installing config.txt"
+
+cp $(dirname $0)/config.txt ${BINARIES_DIR}/config.txt
