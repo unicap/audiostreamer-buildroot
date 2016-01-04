ec3596498a9c64fa157a8f4c0e235ae5f58b1d93
Arne Caspari <arne@unicap-imaging.org>
2016-01-04 20:59:23 +0100

    Generate a directory containing SD card content during post-image

--- board/arne/rpi2/post-image.sh
+++ board/arne/rpi2/post-image.sh
@@ -9,3 +9,16 @@ mv "${BINARIES_DIR}/zImage-dt" "${BINARIES_DIR}/zImage"
 echo "Installing config.txt"
 
 cp $(dirname $0)/config.txt ${BINARIES_DIR}/config.txt
+
+
+EXPORTDIR=${BINARIES_DIR}/sdcard
+
+echo "Generating SD card content in ${EXPORTDIR}"
+
+mkdir -p ${EXPORTDIR}
+cp ${BINARIES_DIR}/config.txt ${EXPORTDIR}/
+cp ${BINARIES_DIR}/rpi-firmware/* ${EXPORTDIR}/ || echo "Error ignored"
+cp ${BINARIES_DIR}/*.dtb ${EXPORTDIR}/
+cp ${BINARIES_DIR}/zImage ${EXPORTDIR}/
+
+echo ">>DONE"
