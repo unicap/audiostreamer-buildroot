c4efd02e807b55d46ebe0b74da4253251a702113
Arne Caspari <arne@unicap-imaging.org>
2016-07-10 21:23:24 +0200

    Add new package Sub::Name

---
+++ package/perl-sub-name/perl-sub-name.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-sub_name
+#
+################################################################################
+
+PERL_SUB_NAME_VERSION = 0.15
+PERL_SUB_NAME_SOURCE = Sub-Name-$(PERL_SUB_NAME_VERSION).tar.gz
+PERL_SUB_NAME_SITE = $(BR2_CPAN_MIRROR)/authors/id/E/ET/ETHER
+PERL_SUB_NAME_LICENSE = Artistic or GPLv1+
+PERL_SUB_NAME_LICENSE_FILES = README
+
+$(eval $(perl-package))
