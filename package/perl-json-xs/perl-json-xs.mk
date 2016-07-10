e2e0ba21c88a3d1b1edcfd92544d7f43a7983857
Arne Caspari <arne@unicap-imaging.org>
2016-07-10 21:22:32 +0200

    Add new package JSON::XS

---
+++ package/perl-json-xs/perl-json-xs.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-json-xs
+#
+################################################################################
+
+PERL_JSON_XS_VERSION = 3.02
+PERL_JSON_XS_SOURCE = JSON-XS-$(PERL_JSON_XS_VERSION).tar.gz
+PERL_JSON_XS_SITE = $(BR2_CPAN_MIRROR)/authors/id/M/ML/MLEHMANN
+PERL_JSON_XS_LICENSE = Artistic or GPLv1+
+PERL_JSON_XS_LICENSE_FILES = README
+
+$(eval $(perl-package))
