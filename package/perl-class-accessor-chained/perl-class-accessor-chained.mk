aa596da47ea40f6e5e556c6e0b4437ade242126a
Arne Caspari <arne@unicap-imaging.org>
2016-07-11 23:31:49 +0200

    Add package Class::Accessor::Chained

---
+++ package/perl-class-accessor-chained/perl-class-accessor-chained.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-class_accessor_chained
+#
+################################################################################
+
+PERL_CLASS_ACCESSOR_CHAINED_VERSION = 0.01
+PERL_CLASS_ACCESSOR_CHAINED_SOURCE = Class-Accessor-Chained-$(PERL_CLASS_ACCESSOR_CHAINED_VERSION).tar.gz
+PERL_CLASS_ACCESSOR_CHAINED_SITE = $(BR2_CPAN_MIRROR)/authors/id/R/RC/RCLAMP
+PERL_CLASS_ACCESSOR_CHAINED_LICENSE = Artistic or GPLv1+
+PERL_CLASS_ACCESSOR_CHAINED_LICENSE_FILES = README
+
+$(eval $(perl-package))
