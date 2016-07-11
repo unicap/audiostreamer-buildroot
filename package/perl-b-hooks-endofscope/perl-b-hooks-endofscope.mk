ff63c8f383af11a867e516abfd271b04eede1e9e
Arne Caspari <arne@unicap-imaging.org>
2016-07-11 23:30:25 +0200

    Add new package B::Hooks::EndOfScope

---
+++ package/perl-b-hooks-endofscope/perl-b-hooks-endofscope.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-b_hooks_endofscope
+#
+################################################################################
+
+PERL_B_HOOKS_ENDOFSCOPE_VERSION = 0.21
+PERL_B_HOOKS_ENDOFSCOPE_SOURCE = B-Hooks-EndOfScope-$(PERL_B_HOOKS_ENDOFSCOPE_VERSION).tar.gz
+PERL_B_HOOKS_ENDOFSCOPE_SITE = $(BR2_CPAN_MIRROR)/authors/id/E/ET/ETHER
+PERL_B_HOOKS_ENDOFSCOPE_LICENSE = Artistic or GPLv1+
+PERL_B_HOOKS_ENDOFSCOPE_LICENSE_FILES = README
+
+$(eval $(perl-package))
