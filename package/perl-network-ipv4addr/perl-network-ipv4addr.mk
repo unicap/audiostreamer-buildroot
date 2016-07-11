702211912e6c84566e033cb7e80be7295e21a336
Arne Caspari <arne@unicap-imaging.org>
2016-07-11 23:41:49 +0200

    Add package Network::IPV4Addr

---
+++ package/perl-network-ipv4addr/perl-network-ipv4addr.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-network_ipv4addr
+#
+################################################################################
+
+PERL_NETWORK_IPV4ADDR_VERSION = 0.05
+PERL_NETWORK_IPV4ADDR_SOURCE = Network-IPv4Addr-$(PERL_NETWORK_IPV4ADDR_VERSION).tar.gz
+PERL_NETWORK_IPV4ADDR_SITE = $(BR2_CPAN_MIRROR)/authors/id/F/FR/FRAJULAC
+PERL_NETWORK_IPV4ADDR_LICENSE = Artistic or GPLv1+
+PERL_NETWORK_IPV4ADDR_LICENSE_FILES = README
+
+$(eval $(perl-package))
