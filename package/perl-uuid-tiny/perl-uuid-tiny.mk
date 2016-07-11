b2c16aeba2aa163d2fbb60ddee651461f16beb4c
Arne Caspari <arne@unicap-imaging.org>
2016-07-11 23:46:02 +0200

    Add package UUID::Tiny

---
+++ package/perl-uuid-tiny/perl-uuid-tiny.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-uuid_tiny
+#
+################################################################################
+
+PERL_UUID_TINY_VERSION = 1.04
+PERL_UUID_TINY_SOURCE = UUID-Tiny-$(PERL_UUID_TINY_VERSION).tar.gz
+PERL_UUID_TINY_SITE = $(BR2_CPAN_MIRROR)/authors/id/C/CA/CAUGUSTIN
+PERL_UUID_TINY_LICENSE = Artistic or GPLv1+
+PERL_UUID_TINY_LICENSE_FILES = README
+
+$(eval $(perl-package))
