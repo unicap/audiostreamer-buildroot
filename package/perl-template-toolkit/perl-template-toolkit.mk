a36cab430ec6da394d8f3243f9a7c524078883d4
Arne Caspari <arne@unicap-imaging.org>
2016-07-11 23:28:46 +0200

    Fix build rule for Template::Toolkit

--- package/perl-template-toolkit/perl-template-toolkit.mk
+++ package/perl-template-toolkit/perl-template-toolkit.mk
@@ -11,10 +11,10 @@ PERL_TEMPLATE_TOOLKIT_LICENSE = Artistic or GPLv1+
 PERL_TEMPLATE_TOOLKIT_LICENSE_FILES = README
 
 define PERL_TEMPLATE_TOOLKIT_BUILD_CMDS
-	sed "s~/usr/bin/gcc~$(TARGET_CC)~" $(@D)/Makefile > $(@D)/Makefile.tmp
-	mv $(@D)/Makefile.tmp $(@D)/Makefile
-	sed "s~/usr/bin/gcc~$(TARGET_CC)~" $(@D)/xs/Makefile > $(@D)/xs/Makefile.tmp
-	mv $(@D)/xs/Makefile.tmp $(@D)/xs/Makefile
+	sed "s~/usr/bin/gcc~$(TARGET_CC) -Os~" -i $(@D)/Makefile
+	sed "s~/usr/bin/gcc~$(TARGET_CC) -Os~" -i $(@D)/xs/Makefile
+	sed "s~x86_64~$(KERNEL_ARCH)~" -i $(@D)/xs/Makefile
+	sed "s~host/usr/lib~staging/usr/lib~" -i $(@D)/xs/Makefile
 	$(MAKE) $(@D)/Makefile -C $(@D)
 endef
 
