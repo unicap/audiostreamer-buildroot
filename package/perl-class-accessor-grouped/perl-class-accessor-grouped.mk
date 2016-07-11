2b8218e790fe6cfb4ffc552d3cc9df76b3401857
Arne Caspari <arne@unicap-imaging.org>
2016-07-11 23:32:31 +0200

    Add package Class::Accessor::Grouped

---
+++ package/perl-class-accessor-grouped/perl-class-accessor-grouped.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-class_accessor_grouped
+#
+################################################################################
+
+PERL_CLASS_ACCESSOR_GROUPED_VERSION = 0.10012
+PERL_CLASS_ACCESSOR_GROUPED_SOURCE = Class-Accessor-Grouped-$(PERL_CLASS_ACCESSOR_GROUPED_VERSION).tar.gz
+PERL_CLASS_ACCESSOR_GROUPED_SITE = $(BR2_CPAN_MIRROR)/authors/id/R/RI/RIBASUSHI
+PERL_CLASS_ACCESSOR_GROUPED_LICENSE = Artistic or GPLv1+
+PERL_CLASS_ACCESSOR_GROUPED_LICENSE_FILES = README
+
+$(eval $(perl-package))
