bf755e4b998a266321af5b7debc302236a1bf1e5
Arne Caspari <arne@unicap-imaging.org>
2016-07-11 23:33:44 +0200

    Add package Class::ISA

---
+++ package/perl-class-isa/perl-class-isa.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-class_isa
+#
+################################################################################
+
+PERL_CLASS_ISA_VERSION = 0.36
+PERL_CLASS_ISA_SOURCE = Class-ISA-$(PERL_CLASS_ISA_VERSION).tar.gz
+PERL_CLASS_ISA_SITE = $(BR2_CPAN_MIRROR)/authors/id/S/SM/SMUELLER
+PERL_CLASS_ISA_LICENSE = Artistic or GPLv1+
+PERL_CLASS_ISA_LICENSE_FILES = README
+
+$(eval $(perl-package))
