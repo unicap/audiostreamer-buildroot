7c5491fe16593c202cb1df74d5807144b9a6ee92
Arne Caspari <arne@unicap-imaging.org>
2016-07-11 23:33:25 +0200

    Class::Inspector

---
+++ package/perl-class-inspector/perl-class-inspector.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-class_inspector
+#
+################################################################################
+
+PERL_CLASS_INSPECTOR_VERSION = 1.28
+PERL_CLASS_INSPECTOR_SOURCE = Class-Inspector-$(PERL_CLASS_INSPECTOR_VERSION).tar.gz
+PERL_CLASS_INSPECTOR_SITE = $(BR2_CPAN_MIRROR)/authors/id/A/AD/ADAMK
+PERL_CLASS_INSPECTOR_LICENSE = Artistic or GPLv1+
+PERL_CLASS_INSPECTOR_LICENSE_FILES = README
+
+$(eval $(perl-package))
