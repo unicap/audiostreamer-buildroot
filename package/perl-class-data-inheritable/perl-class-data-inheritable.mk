f2e6b4687aa95e91678aedd5e3a5c48dc23bdb48
Arne Caspari <arne@unicap-imaging.org>
2016-07-11 23:33:13 +0200

    Add package Class::Data:Inheritable

---
+++ package/perl-class-data-inheritable/perl-class-data-inheritable.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-class_data_inheritable
+#
+################################################################################
+
+PERL_CLASS_DATA_INHERITABLE_VERSION = 0.08
+PERL_CLASS_DATA_INHERITABLE_SOURCE = Class-Data-Inheritable-$(PERL_CLASS_DATA_INHERITABLE_VERSION).tar.gz
+PERL_CLASS_DATA_INHERITABLE_SITE = $(BR2_CPAN_MIRROR)/authors/id/T/TM/TMTM
+PERL_CLASS_DATA_INHERITABLE_LICENSE = Artistic or GPLv1+
+PERL_CLASS_DATA_INHERITABLE_LICENSE_FILES = README
+
+$(eval $(perl-package))
