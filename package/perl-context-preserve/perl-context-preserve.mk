6c710504abaeca7bc87af83b97c868054cea3bdd
Arne Caspari <arne@unicap-imaging.org>
2016-07-11 23:34:11 +0200

    Context::Preserve

---
+++ package/perl-context-preserve/perl-context-preserve.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# perl-context_preserve
+#
+################################################################################
+
+PERL_CONTEXT_PRESERVE_VERSION = 0.01
+PERL_CONTEXT_PRESERVE_SOURCE = Context-Preserve-$(PERL_CONTEXT_PRESERVE_VERSION).tar.gz
+PERL_CONTEXT_PRESERVE_SITE = $(BR2_CPAN_MIRROR)/authors/id/J/JR/JROCKWAY
+PERL_CONTEXT_PRESERVE_LICENSE = Artistic or GPLv1+
+PERL_CONTEXT_PRESERVE_LICENSE_FILES = README
+
+$(eval $(perl-package))
