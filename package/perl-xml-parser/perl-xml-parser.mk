b8210c4b0159e41814f09fdd06dfbf155760e5e6
Arne Caspari <arne@unicap-imaging.org>
2016-07-12 21:52:36 +0200

    Add dependency for XML::Parser

--- package/perl-xml-parser/perl-xml-parser.mk
+++ package/perl-xml-parser/perl-xml-parser.mk
@@ -1,12 +1,13 @@
 ################################################################################
 #
-# perl-xml_parser
+# perl-xml-parser
 #
 ################################################################################
 
 PERL_XML_PARSER_VERSION = 2.44
 PERL_XML_PARSER_SOURCE = XML-Parser-$(PERL_XML_PARSER_VERSION).tar.gz
 PERL_XML_PARSER_SITE = $(BR2_CPAN_MIRROR)/authors/id/T/TO/TODDR
+PERL_XML_PARSER_DEPENDENCIES = perl-libwww-perl
 PERL_XML_PARSER_LICENSE = Artistic or GPLv1+
 PERL_XML_PARSER_LICENSE_FILES = README
 
@@ -17,6 +18,5 @@ define PERL_XML_PARSER_BUILD_CMDS
 	sed "s~host/usr/lib~staging/usr/lib~" -i $(@D)/Expat/Makefile
 	$(MAKE) $(@D)/Makefile -C $(@D)
 endef
-
 
 $(eval $(perl-package))
