b87df332a05ecdd9ddae84d625bbe86fd9e64dc3
Arne Caspari <arne@unicap-imaging.org>
2016-07-12 21:51:19 +0200

    Update package Class::Accessor::Chained

--- package/perl-class-accessor-chained/perl-class-accessor-chained.mk
+++ package/perl-class-accessor-chained/perl-class-accessor-chained.mk
@@ -1,12 +1,13 @@
 ################################################################################
 #
-# perl-class_accessor_chained
+# perl-class-accessor-chained
 #
 ################################################################################
 
 PERL_CLASS_ACCESSOR_CHAINED_VERSION = 0.01
 PERL_CLASS_ACCESSOR_CHAINED_SOURCE = Class-Accessor-Chained-$(PERL_CLASS_ACCESSOR_CHAINED_VERSION).tar.gz
 PERL_CLASS_ACCESSOR_CHAINED_SITE = $(BR2_CPAN_MIRROR)/authors/id/R/RC/RCLAMP
+PERL_CLASS_ACCESSOR_CHAINED_DEPENDENCIES = perl-class-accessor host-perl-module-build
 PERL_CLASS_ACCESSOR_CHAINED_LICENSE = Artistic or GPLv1+
 PERL_CLASS_ACCESSOR_CHAINED_LICENSE_FILES = README
 
