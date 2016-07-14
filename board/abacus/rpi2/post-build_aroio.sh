bb7c57237d0a36bd3c22f8cfe572c7382552b69a
Arne Caspari <arne@unicap-imaging.org>
2016-07-14 10:43:38 +0200

    Generate version info from latest git commit
    Store version information of the latest git commit in /VERSION and a
    version timestamp /VERSION_TIMESTAMP.

--- board/abacus/rpi2/post-build_aroio.sh
+++ board/abacus/rpi2/post-build_aroio.sh
@@ -6,3 +6,7 @@ if [ -e userconfig_default.txt ]; then
     echo "Custom userconfig found, copying to target"
     cp userconfig_default.txt ${TARGET_DIR}/etc
 fi
+
+echo -n $(git log --format="Commit: %h from %aD" -n 1) > ${TARGET_DIR}/VERSION
+echo -n $(git log --format="%ct" -n 1) > ${TARGET_DIR}/VERSION_TIMESTAMP
+
