7462cea80d9ddf6f90c30275dd3d666f34b8f95d
Arne Caspari <arne@unicap-imaging.org>
2016-07-28 20:27:47 +0200

    Build a real SD-Card image

--- board/abacus/rpi2/post-image_aroio.sh
+++ board/abacus/rpi2/post-image_aroio.sh
@@ -23,4 +23,27 @@ cp -r ${DISTDIR}/* ${EXPORTDIR}/
 
 rm -f ${EXPORTDIR}/cmdline.txt
 
+
+BOARD_DIR="$(dirname $0)"
+BOARD_NAME="$(basename ${BOARD_DIR})"
+GENIMAGE_CFG="${BOARD_DIR}/genimage-aroio.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+# Mark the kernel as DT-enabled
+mkdir -p "${BINARIES_DIR}/kernel-marked"
+${HOST_DIR}/usr/bin/mkknlimg "${BINARIES_DIR}/zImage" \
+	"${BINARIES_DIR}/kernel-marked/zImage"
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage                           \
+	--rootpath "${TARGET_DIR}"     \
+	--tmppath "${GENIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${GENIMAGE_CFG}"
+
+
 echo ">>DONE"
+
+exit $?
