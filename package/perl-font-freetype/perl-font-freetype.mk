1492cafd4e2e1809f78c0a8c524c1319e8494bdf
Arne Caspari <arne@unicap-imaging.org>
2016-09-13 16:04:13 +0200

    perl-font-freetype: Use correct freetype-config

--- package/perl-font-freetype/perl-font-freetype.mk
+++ package/perl-font-freetype/perl-font-freetype.mk
@@ -10,5 +10,9 @@ PERL_FONT_FREETYPE_SITE = $(BR2_CPAN_MIRROR)/authors/id/D/DM/DMOL
 PERL_FONT_FREETYPE_LICENSE = Artistic or GPLv1+
 PERL_FONT_FREETYPE_LICENSE_FILES = README
 PERL_FONT_FREETYPE_DEPENDENCIES = freetype host-perl-devel-checklib host-perl-file-which
+define PERL_FONT_FREETYPE_PRE_CONFIGURE_FIXUP
+	(cd $(@D); sed -i s+freetype-config+$(STAGING_DIR)/usr/bin/freetype-config+g Makefile.PL)
+endef
+PERL_FONT_FREETYPE_PRE_CONFIGURE_HOOKS += PERL_FONT_FREETYPE_PRE_CONFIGURE_FIXUP
 
 $(eval $(perl-package))
