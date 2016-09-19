5b0b95f0711ed6a7721859e1ed2ce417c753b291
Arne Caspari <arne@unicap-imaging.org>
2016-09-19 17:24:19 +0200

    Add more logging shell functions

--- board/abacus/rpi2/overlay_aroio/opt/shutils.sh
+++ board/abacus/rpi2/overlay_aroio/opt/shutils.sh
@@ -16,3 +16,33 @@ cmark() {
     echo -n `uptime -s` | tee -a /tmp/shell.log
     clog "-------------- "
 }
+
+cmarkn() {
+    clog -n "-------------- "
+    echo -n `uptime -s` >> /tmp/shell.log
+    clog "-------------- "
+}
+
+cmarks() {
+    clog -n "-------------- [$1] "
+    echo -n `uptime -s` | tee -a /tmp/shell.log
+    clog "-------------- "
+}
+
+cmarksn() {
+    clog -n "-------------- [$1] "
+    echo -n `uptime -s` >> /tmp/shell.log
+    clog "-------------- "
+}
+
+logstart() {
+    cmarks $(basename $1)
+    $* | tee -a /tmp/shell.log
+    cmarks "Finished: $(basename $1)"
+}
+
+logstartn() {
+    cmarksn $(basename $1)
+    $* 2>&1 >> /tmp/shell.log
+    cmarksn "Finished: $(basename $1)"
+}
