c420e284563535e3817d6e93833d90c2e1f34291
Arne Caspari <arne@unicap-imaging.org>
2016-10-01 11:21:38 +0200

    Clean up init.d/ files after build

--- board/abacus/rpi2/post-build_aroio.sh
+++ board/abacus/rpi2/post-build_aroio.sh
@@ -10,6 +10,22 @@ fi
 echo -n $(git log --format="Commit: %h from %aD" -n 1) > ${TARGET_DIR}/VERSION
 echo -n $(git log --format="%ct" -n 1) > ${TARGET_DIR}/VERSION_TIMESTAMP
 
+echo "Cleaning up init scripts"
+if [ -e ${TARGET_DIR}/etc/init.d/S05avahi-setup.sh ]; then
+    mv ${TARGET_DIR}/etc/init.d/S05avahi-setup.sh ${TARGET_DIR}/etc/init.d/avahi-setup
+    mv ${TARGET_DIR}/etc/init.d/S50avahi-daemon ${TARGET_DIR}/etc/init.d/avahi-daemon
+fi
+
+if [ -e ${TARGET_DIR}/etc/init.d/S30dbus ]; then
+    mv ${TARGET_DIR}/etc/init.d/S30dbus ${TARGET_DIR}/etc/init.d/dbus
+fi
+
+if [ ! "${TARGET_DIR}" = "" ]; then
+    echo "Removing:"
+    ls ${TARGET_DIR}/etc/init.d/S[0-9]* || true
+    rm -f ${TARGET_DIR}/etc/init.d/S[0-9]*
+fi
+
 # create required empty directories
 
 mkdir -p ${TARGET_DIR}/mnt/mmcblk0p1
