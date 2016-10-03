0abe793e60184b9077e0c0d5c1ad15d011676592
Arne Caspari <arne@unicap-imaging.org>
2016-10-03 09:40:14 +0200

    Use correct gmrender-resurrect version

--- package/gmrender-resurrect/gmrender-resurrect.mk
+++ package/gmrender-resurrect/gmrender-resurrect.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-GMRENDER_RESURRECT_VERSION = 48caaa4f6c386fd1586126c801cd326f96d5fa5c
-GMRENDER_RESURRECT_SITE = $(call github,hzeller,gmrender-resurrect,$(GMRENDER_RESURRECT_VERSION))
+GMRENDER_RESURRECT_VERSION = 04390e4dade8595527dd157dddeb98a6c6047871
+GMRENDER_RESURRECT_SITE = $(call github,unicap,gmrender-resurrect,$(GMRENDER_RESURRECT_VERSION))
 # Original distribution does not have default configure,
 # so we need to autoreconf:
 GMRENDER_RESURRECT_AUTORECONF = YES
 GMRENDER_RESURRECT_LICENSE = GPLv2+
 GMRENDER_RESURRECT_LICENSE_FILES = COPYING
-GMRENDER_RESURRECT_DEPENDENCIES = gstreamer1 libupnp
+GMRENDER_RESURRECT_DEPENDENCIES = gstreamer1 libupnp jack2
 
 $(eval $(autotools-package))
