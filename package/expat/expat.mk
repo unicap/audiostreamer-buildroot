35f794f1e80d4a7af6165ed647caf1a55579b642
Arne Caspari <arne@unicap-imaging.org>
2018-06-03 10:49:59 +0200

    Add host-perl dependency to expat

--- package/expat/expat.mk
+++ package/expat/expat.mk
@@ -9,7 +9,7 @@ EXPAT_SITE = http://downloads.sourceforge.net/project/expat/expat/$(EXPAT_VERSIO
 EXPAT_SOURCE = expat-$(EXPAT_VERSION).tar.xz
 EXPAT_INSTALL_STAGING = YES
 EXPAT_DEPENDENCIES = host-pkgconf
-HOST_EXPAT_DEPENDENCIES = host-pkgconf
+HOST_EXPAT_DEPENDENCIES = host-pkgconf host-perl
 EXPAT_LICENSE = MIT
 EXPAT_LICENSE_FILES = COPYING
 
