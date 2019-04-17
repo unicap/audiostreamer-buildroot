bd098439bd37460f580209ace0f2557502f985f5
Mr. Berndt <nb@komeda-berlin.de>
2019-04-17 17:35:31 +0200

    RTL8812 driver update tomatch current kernel version.

--- package/rtl8821au/rtl8821au.mk
+++ package/rtl8821au/rtl8821au.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-RTL8821AU_VERSION = fb91faf 
+#RTL8821AU_VERSION = fb91faf
+RTL8821AU_VERSION = 9f09c76
 RTL8821AU_SITE = $(call github,abperiasamy,rtl8812AU_8821AU_linux,$(RTL8821AU_VERSION))
 RTL8821AU_LICENSE = GPL-2.0
 RTL8821AU_LICENSE_FILES = LICENSE
