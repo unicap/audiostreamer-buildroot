d2079122cda88ef58359bbf5a80ec37e85a1117e
mr-berndt <nb@komeda-berlin.de>
2019-06-19 11:25:33 +0200

    System: Only changed a comment it .mk file.

--- package/gmrender-resurrect/gmrender-resurrect.mk
+++ package/gmrender-resurrect/gmrender-resurrect.mk
@@ -10,6 +10,10 @@ GMRENDER_RESURRECT_VERSION = 33600ab663f181c4f4f5c48aba25bf961760a300
 # uralt
 #GMRENDER_RESURRECT_VERSION = 400361649f3d540c08001c0ad68222388e65be67
 GMRENDER_RESURRECT_SITE = $(call github,hzeller,gmrender-resurrect,$(GMRENDER_RESURRECT_VERSION))
+
+# 8/2018
+GMRENDER_RESURRECT_VERSION = a7b0b1b9ca482d2d34ac62c2f2dc0cf0dfbb702b
+
 # Original distribution does not have default configure,
 # so we need to autoreconf:
 GMRENDER_RESURRECT_AUTORECONF = YES
