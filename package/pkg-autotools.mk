4037453e343e101f78791ecd11a60196aae4553e
Arne Caspari <arne.caspari@theimagingsource.com>
2020-06-07 21:23:30 +0200

    Add GIR path to autotools

--- package/pkg-autotools.mk
+++ package/pkg-autotools.mk
@@ -162,6 +162,7 @@ endif
 
 $(2)_CONF_ENV			?=
 $(2)_CONF_OPTS			?=
+$(2)_GIR_EXTRA_LIBS_PATH	?=
 $(2)_MAKE_ENV			?=
 $(2)_MAKE_OPTS			?=
 $(2)_INSTALL_OPTS                ?= install
@@ -239,6 +240,8 @@ define $(2)_CONFIGURE_CMDS
 endef
 endif
 endif
+
+export GIR_EXTRA_LIBS_PATH=$$($$(PKG)_GIR_EXTRA_LIBS_PATH)
 
 $(2)_POST_PATCH_HOOKS += UPDATE_CONFIG_HOOK
 
